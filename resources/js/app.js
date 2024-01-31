0754b645f2818736220aa4765858837b0a167f51
anggakusumap <kusuma_putra@student.unud.ac.id>
2024-01-31 17:43:29 +0800

    push - 1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-console.log("Hello from app.js");
+document.addEventListener("DOMContentLoaded", function () {
+  var header = document.querySelector("header");
+
+  // Add 'header-transparent' class initially
+  header.classList.add("header-transparent");
+
+  // Check scroll position on scroll event
+  window.addEventListener("scroll", function () {
+    // When the scroll position is at the top, make the header transparent
+    if (window.scrollY === 0) {
+      header.classList.add("header-transparent");
+    } else {
+      // Otherwise, remove the transparent class to make the header sticky
+      header.classList.remove("header-transparent");
+    }
+  });
+});
